8125c0d65a3ec6ed3feee8cb53d7235537081832
debych <zh.eliseev@yandex.ru>
2022-08-31 18:34:50 +0300

    lint every .ts file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
     root: true,
     overrides: [
         {
-            files: ['test/**/*.ts'],
+            files: ['./**/*.ts'],
             parser: '@typescript-eslint/parser',
             parserOptions: {
                 project: './tsconfig.json',
                 tsconfigRootDir: __dirname,
-                createDefaultProgram: true
+                createDefaultProgram: true,
+                sourceType: "module"
             },
             plugins: [
                 '@typescript-eslint',
